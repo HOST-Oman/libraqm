6565afa836af358ab8322b248e961729137ec957
abderraouf-adjal <abderraouf.adjal@gmail.com>
2015-12-18 18:41:40 +0100

    drop unrelated stylistic changes

--- raqm.c
+++ raqm.c
@@ -35,25 +35,27 @@
 typedef struct _FriBidiRunStruct FriBidiRun;
 struct _FriBidiRunStruct
 {
-    FriBidiRun *prev;
-    FriBidiRun *next;
+  FriBidiRun *prev;
+  FriBidiRun *next;
 
-    FriBidiStrIndex pos, len;
-    FriBidiCharType type;
-    FriBidiLevel level;
+  FriBidiStrIndex pos, len;
+  FriBidiCharType type;
+  FriBidiLevel level;
 };
 
 /* Reverse an array of runs */
 static void reverse_run (FriBidiRun *arr, const FriBidiStrIndex len)
 {
-    FriBidiStrIndex i;
-    assert (arr);
-    for (i = 0; i < len/2; i++)
-    {
-        FriBidiRun temp = arr[i];
-        arr[i] = arr[len - 1 - i];
-        arr[len - 1 - i] = temp;
-    }
+  FriBidiStrIndex i;
+
+  assert (arr);
+
+  for (i = 0; i < len/2; i++)
+  {
+    FriBidiRun temp = arr[i];
+    arr[i] = arr[len - 1 - i];
+    arr[len - 1 - i] = temp;
+  }
 }
 
 /* Seperates and reorders runs via fribidi using bidi algorithm*/
@@ -66,92 +68,92 @@ FriBidiStrIndex fribidi_reorder_runs (
   FriBidiLevel *embedding_levels,
   /* output */
   FriBidiRun *runs
-  )
+)
 {
-    FriBidiLevel level;
-    FriBidiLevel last_level = -1;
-    FriBidiLevel max_level = 0;
-    FriBidiStrIndex i;
-    FriBidiStrIndex run_count = 0;
-    FriBidiStrIndex run_start = 0;
-    FriBidiStrIndex run_index = 0;
+  FriBidiLevel level;
+  FriBidiLevel last_level = -1;
+  FriBidiLevel max_level = 0;
+  FriBidiStrIndex i;
+  FriBidiStrIndex run_count = 0;
+  FriBidiStrIndex run_start = 0;
+  FriBidiStrIndex run_index = 0;
 
-    if (len == 0)
+  if (len == 0)
+  {
+    goto out;
+  }
+
+  assert (bidi_types);
+  assert (embedding_levels);
+
+  {
+    /* L1. Reset the embedding levels of some chars:
+       4. any sequence of white space characters at the end of the line. */
+    for (i = len - 1; i >= 0 &&
+       FRIBIDI_IS_EXPLICIT_OR_BN_OR_WS (bidi_types[i]); i--)
     {
-        goto out;
+      embedding_levels[i] = FRIBIDI_DIR_TO_LEVEL (base_dir);
+    }
+  }
+  /* Find max_level of the line.  We don't reuse the paragraph
+  * max_level, both for a cleaner API, and that the line max_level
+  * may be far less than paragraph max_level. */
+  for (i = len - 1; i >= 0; i--)
+  {
+    if (embedding_levels[i] > max_level)
+    {
+      max_level = embedding_levels[i];
+    }
+  }
+
+  for (i = 0; i < len; i++)
+  {
+    if (embedding_levels[i] != last_level)
+    {
+      run_count++;
     }
 
-    assert (bidi_types);
-    assert (embedding_levels);
+    last_level = embedding_levels[i];
+  }
 
+  if (runs == NULL)
+  {
+    goto out;
+  }
+
+
+  while (run_start < len)
+  {
+    int runLength = 0;
+    while ((run_start + runLength) < len && embedding_levels[run_start] == embedding_levels[run_start + runLength])
     {
-        /* L1. Reset the embedding levels of some chars:
-           4. any sequence of white space characters at the end of the line. */
-        for (i = len - 1; i >= 0 &&
-             FRIBIDI_IS_EXPLICIT_OR_BN_OR_WS (bidi_types[i]); i--)
-        {
-            embedding_levels[i] = FRIBIDI_DIR_TO_LEVEL (base_dir);
-        }
-    }
-    /* Find max_level of the line.  We don't reuse the paragraph
-    * max_level, both for a cleaner API, and that the line max_level
-    * may be far less than paragraph max_level. */
-    for (i = len - 1; i >= 0; i--)
-    {
-        if (embedding_levels[i] > max_level)
-        {
-            max_level = embedding_levels[i];
-        }
+      runLength++;
     }
 
-    for (i = 0; i < len; i++)
+    runs[run_index].pos = run_start;
+    runs[run_index].level = embedding_levels[run_start];
+    runs[run_index].len = runLength;
+    run_start += runLength;
+    run_index++;
+  }
+
+  /* L2. Reorder. */
+  for (level = max_level; level > 0; level--)
+  {
+    for (i = run_count - 1; i >= 0; i--)
     {
-        if (embedding_levels[i] != last_level)
-        {
-            run_count++;
-        }
-
-        last_level = embedding_levels[i];
+      if (runs[i].level >= level)
+      {
+        int end = i;
+          for (i--; (i >= 0 && runs[i].level >= level); i--)
+          ;
+        reverse_run (runs + i + 1, end - i);
+      }
     }
-
-    if (runs == NULL)
-    {
-        goto out;
-    }
-
-
-    while (run_start < len)
-    {
-        int runLength = 0;
-        while ((run_start + runLength) < len && embedding_levels[run_start] == embedding_levels[run_start + runLength])
-        {
-            runLength++;
-        }
-
-        runs[run_index].pos = run_start;
-        runs[run_index].level = embedding_levels[run_start];
-        runs[run_index].len = runLength;
-        run_start += runLength;
-        run_index++;
-    }
-
-    /* L2. Reorder. */
-    for (level = max_level; level > 0; level--)
-    {
-        for (i = run_count - 1; i >= 0; i--)
-        {
-            if (runs[i].level >= level)
-            {
-                int end = i;
-                for (i--; (i >= 0 && runs[i].level >= level); i--)
-                  ;
-                reverse_run (runs + i + 1, end - i);
-            }
-        }
-    }
+  }
 
 out:
-    return run_count;
+  return run_count;
 }
 
 #endif
@@ -290,7 +292,8 @@ itemize_by_script(int bidirun_count,
                  FriBidiRun *bidiruns,
                  Run *runs)
 {
-    int i, j, run_count = 0;
+    int i, j;
+    int run_count = 0;
 
     /* To get number of runs after script seperation */
     for (i = 0; i < bidirun_count; i++)
