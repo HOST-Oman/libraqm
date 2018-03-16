HOST-Oman/libraqm

commit e4c60ac96161cb05b951e0bbcb31e35f37dcb132

The error value is 0 not < 0

src/raqm.c
@@ -1093,7 +1093,7 @@ _raqm_itemize (raqm_t *rq)
      rq->resolved_dir = RAQM_DIRECTION_RTL;
   }
 
-  if (max_level < 0)
+  if (max_level == 0)
   {
     ok = false;
     goto done;

commit 7088743be4afe22e82282912bf414f5051046bb8

Make TTB direction work again
Regression from e4c60ac96161cb05b951e0bbcb31e35f37dcb132. Also add a
basic test to not break it again.

Fixes https://github.com/HOST-Oman/libraqm/issues/81

src/raqm.c
@@ -1093,7 +1093,7 @@ _raqm_itemize (raqm_t *rq)
   if (rq->base_dir == RAQM_DIRECTION_TTB)
   {
     /* Treat every thing as LTR in vertical text */
-    max_level = 0;
+    max_level = 1;
     memset (types, FRIBIDI_TYPE_LTR, rq->text_len);
     memset (levels, 0, rq->text_len);
     rq->resolved_dir = RAQM_DIRECTION_LTR;

tests/Makefile.am
@@ -38,6 +38,7 @@ TEST_FILES = \
     direction-rtl-3.test \
     direction-rtl-4.test \
     direction-rtl-5.test \
+    direction-ttb-1.test \
     empty-text.test \
     features-arabic.test \
     features-kerning.test \

tests/direction-ttb-1.test
@@ -1,0 +1,52 @@
+fonts/sha1sum/788742748bf8bfbd3b6b56859f92938275da74bd.otf
+汉语English
+--direction=ttb
+Text is: 汉语English
+Direction is: TTB
+
+Before script detection:
+script for ch[0]	Hani
+script for ch[1]	Hani
+script for ch[2]	Latn
+script for ch[3]	Latn
+script for ch[4]	Latn
+script for ch[5]	Latn
+script for ch[6]	Latn
+script for ch[7]	Latn
+script for ch[8]	Latn
+
+After script detection:
+script for ch[0]	Hani
+script for ch[1]	Hani
+script for ch[2]	Latn
+script for ch[3]	Latn
+script for ch[4]	Latn
+script for ch[5]	Latn
+script for ch[6]	Latn
+script for ch[7]	Latn
+script for ch[8]	Latn
+
+Number of runs before script itemization: 1
+
+Fribidi Runs:
+run[0]:	 start: 0	length: 9	level: 0
+
+Number of runs after script itemization: 2
+
+Final Runs:
+run[0]:	 start: 0	length: 2	direction: ttb	script: Hani	font: Noto Serif CJK SC
+run[1]:	 start: 2	length: 7	direction: ttb	script: Latn	font: Noto Serif CJK SC
+
+Glyph information:
+glyph [8]	x_offset: -500	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [9]	x_offset: -500	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [1]	x_offset: -327	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [6]	x_offset: -329	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [2]	x_offset: -280	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [5]	x_offset: -162	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [4]	x_offset: -162	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [7]	x_offset: -235	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+glyph [3]	x_offset: -327	y_offset: -880	x_advance: 0	font: Noto Serif CJK SC
+
+UTF-32 clusters: 00 01 02 03 04 05 06 07 08
+UTF-8 clusters:  00 03 06 07 08 09 10 11 12