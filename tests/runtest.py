14a063c4d59833afdf752b25a5ed1e3ecb063534
Khaled Hosny <khaledhosny@eglug.org>
2018-09-18 21:06:34 +0200

    Port test runner script to Python

---
+++ tests/runtest.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+
+import ast
+import difflib
+import os
+import subprocess
+import sys
+import io
+
+
+def cmd(command):
+    p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    p.wait()
+    return p.stdout.read().decode("utf-8").strip(), p.returncode
+
+
+def diff(expected, actual):
+    expected = expected.splitlines(1)
+    actual = actual.splitlines(1)
+    diff = difflib.unified_diff(expected, actual)
+    return "".join(diff)
+
+
+srcdir = os.environ.get("srcdir", ".")
+builddir = os.environ.get("builddir", ".")
+testtool = os.path.join(builddir, "raqm-test")
+
+fails = 0
+for filename in sys.argv[1:]:
+    print("Testing %s..." % filename)
+
+    with io.open(filename, newline="") as fp:
+        lines = [l.strip("\n") for l in fp.readlines()]
+    font = lines[0]
+    text = ast.literal_eval("'%s'" % lines[1])
+    opts = lines[2] and lines[2].split(" ") or []
+    expected = "\n".join(lines[3:])
+    if "," in font:
+        fonts = []
+        for f in font.split(","):
+            if f.endswith(".ttf"):
+                f = os.path.join(srcdir, f)
+            fonts.append(f)
+        fonts = ",".join(fonts)
+        command = [testtool, "--text", text] + opts + ["--fonts", fonts]
+    else:
+        font = os.path.join(srcdir, font)
+        command = [testtool, "--text", text] + opts + ["--font", font]
+
+    actual, ret = cmd(command)
+    expected = expected.strip()
+    actual = actual.strip()
+    if ret or actual != expected:
+        print(diff(expected, actual))
+        fails += 1
+
+if fails:
+    print("%d tests failed." % fails)
+    sys.exit(1)
+else:
+    print("All tests passed.")
+    sys.exit(0)
